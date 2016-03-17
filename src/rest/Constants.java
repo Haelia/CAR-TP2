0695461a43e19d8b1aa40087c534a810537b0eea
Kaendan <tristan.camus@gmail.com>
2016-03-17 21:34:48 +0100

    Préparation du rendu

--- src/rest/Constants.java
+++ src/rest/Constants.java
@@ -4,7 +4,4 @@ public class Constants {
 
 	public static String host = "192.168.43.1";
 	public static int port = 1234;
-
-	public static String username = "azerty";
-	public static String password = "azerty";
 }
